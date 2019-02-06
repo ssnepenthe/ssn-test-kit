57c508cba9701ca14f654e73000ce6b2e4940121
ssnepenthe <ssnepenthe@users.noreply.github.com>
2019-02-06 15:10:37 -0700

    Initial CS fixer run

--- src/Response.php
+++ src/Response.php
@@ -9,164 +9,185 @@ use Symfony\Component\Panther\DomCrawler\Crawler as PantherDomCrawler;
 
 class Response
 {
-	protected $crawler;
-	protected $response;
+    protected $crawler;
+    protected $response;
 
-	public function __construct( BrowserKitResponse $response, DomCrawler $crawler ) {
-		$this->response = $response;
-		$this->crawler = $crawler;
-	}
+    public function __construct(BrowserKitResponse $response, DomCrawler $crawler)
+    {
+        $this->response = $response;
+        $this->crawler = $crawler;
+    }
 
-	public function content() {
-		return $this->response->getContent();
-	}
+    public function content()
+    {
+        return $this->response->getContent();
+    }
 
-	public function status() {
-		$this->throwForPanther( 'status' );
+    public function status()
+    {
+        $this->throwForPanther('status');
 
-		return $this->response->getStatus();
-	}
+        return $this->response->getStatus();
+    }
 
-	public function header( string $header, bool $first = true ) {
-		$this->throwForPanther( 'headers' );
+    public function header(string $header, bool $first = true)
+    {
+        $this->throwForPanther('headers');
 
-		return $this->response->getHeader( $header, $first );
-	}
+        return $this->response->getHeader($header, $first);
+    }
 
-	public function headers() {
-		$this->throwForPanther( 'headers' );
+    public function headers()
+    {
+        $this->throwForPanther('headers');
 
-		return $this->response->getHeaders();
-	}
+        return $this->response->getHeaders();
+    }
 
-	public function crawler() {
-		return $this->crawler;
-	}
+    public function crawler()
+    {
+        return $this->crawler;
+    }
 
-	public function unwrap() {
-		return $this->response;
-	}
+    public function unwrap()
+    {
+        return $this->response;
+    }
 
-	public function isPanther() {
-		return $this->crawler instanceof PantherDomCrawler;
-	}
+    public function isPanther()
+    {
+        return $this->crawler instanceof PantherDomCrawler;
+    }
 
-	protected function throwForPanther( string $property )
-	{
-		if ( $this->isPanther() ) {
-			// @todo Better to throw SkippedTestError for PHPUnit's sake?
-			throw new \RuntimeException(
-				"Response {$property} is not available for inspection when using Panther"
-			);
-		}
-	}
+    protected function throwForPanther(string $property)
+    {
+        if ($this->isPanther()) {
+            // @todo Better to throw SkippedTestError for PHPUnit's sake?
+            throw new \RuntimeException(
+                "Response {$property} is not available for inspection when using Panther"
+            );
+        }
+    }
 
-	public function isInformational()
-	{
-		return $this->status() >= 100 && $this->status() < 200;
-	}
+    public function isInformational()
+    {
+        return $this->status() >= 100 && $this->status() < 200;
+    }
 
-	public function isSuccessful()
-	{
-		return $this->status() >= 200 && $this->status() < 300;
-	}
+    public function isSuccessful()
+    {
+        return $this->status() >= 200 && $this->status() < 300;
+    }
 
-	public function isRedirection()
-	{
-		return $this->status() >= 300 && $this->status() < 400;
-	}
+    public function isRedirection()
+    {
+        return $this->status() >= 300 && $this->status() < 400;
+    }
 
-	public function isClientError()
-	{
-		return $this->status() >= 400 && $this->status() < 500;
-	}
+    public function isClientError()
+    {
+        return $this->status() >= 400 && $this->status() < 500;
+    }
 
-	public function isServerError()
-	{
-		return $this->status() >= 500 && $this->status() < 600;
-	}
+    public function isServerError()
+    {
+        return $this->status() >= 500 && $this->status() < 600;
+    }
 
-	public function isOk() {
-		return 200 === $this->status();
-	}
+    public function isOk()
+    {
+        return 200 === $this->status();
+    }
 
-	public function isForbidden() {
-		return 403 === $this->status();
-	}
+    public function isForbidden()
+    {
+        return 403 === $this->status();
+    }
 
-	public function isNotFound() {
-		return 404 === $this->status();
-	}
+    public function isNotFound()
+    {
+        return 404 === $this->status();
+    }
 
-	public function isRedirect( string $location = null ) {
-		return \in_array( $this->status(), [ 201, 301, 302, 303, 307, 308 ], true ) && (
-			null === $location ?: $location === $this->header( 'Location' )
-		);
-	}
+    public function isRedirect(string $location = null)
+    {
+        return \in_array($this->status(), [ 201, 301, 302, 303, 307, 308 ], true) && (
+            null === $location ?: $location === $this->header('Location')
+        );
+    }
 
-	public function assertInformational() {
-		Assert::assertTrue(
-			$this->isInformational(),
-			"Response status {$this->status()} is not an informational status code"
-		);
+    public function assertInformational()
+    {
+        Assert::assertTrue(
+            $this->isInformational(),
+            "Response status {$this->status()} is not an informational status code"
+        );
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function assertSuccessful() {
-		Assert::assertTrue(
-			$this->isSuccessful(),
-			"Response status {$this->status()} is not a successful status code"
-		);
+    public function assertSuccessful()
+    {
+        Assert::assertTrue(
+            $this->isSuccessful(),
+            "Response status {$this->status()} is not a successful status code"
+        );
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function assertRedirection() {
-		Assert::assertTrue(
-			$this->isRedirection(),
-			"Response status {$this->status()} is not a redirection status code"
-		);
+    public function assertRedirection()
+    {
+        Assert::assertTrue(
+            $this->isRedirection(),
+            "Response status {$this->status()} is not a redirection status code"
+        );
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function assertClientError() {
-		Assert::assertTrue(
-			$this->isClientError(),
-			"Response status {$this->status()} is not a client error status code"
-		);
+    public function assertClientError()
+    {
+        Assert::assertTrue(
+            $this->isClientError(),
+            "Response status {$this->status()} is not a client error status code"
+        );
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function assertServerError() {
-		Assert::assertTrue(
-			$this->isServerError(),
-			"Response status {$this->status()} is not a server error status code"
-		);
+    public function assertServerError()
+    {
+        Assert::assertTrue(
+            $this->isServerError(),
+            "Response status {$this->status()} is not a server error status code"
+        );
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function assertStatus( int $status ) {
-		Assert::assertTrue(
-			$status === $this->status(),
-			"Response code {$this->status()} does not match expected {$status} status code"
-		);
+    public function assertStatus(int $status)
+    {
+        Assert::assertTrue(
+            $status === $this->status(),
+            "Response code {$this->status()} does not match expected {$status} status code"
+        );
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function assertOk() {
-		return $this->assertStatus( 200 );
-	}
+    public function assertOk()
+    {
+        return $this->assertStatus(200);
+    }
 
-	public function assertForbidden() {
-		return $this->assertStatus( 403 );
-	}
+    public function assertForbidden()
+    {
+        return $this->assertStatus(403);
+    }
 
-	public function assertNotFound() {
-		return $this->assertStatus( 404 );
-	}
+    public function assertNotFound()
+    {
+        return $this->assertStatus(404);
+    }
 }
