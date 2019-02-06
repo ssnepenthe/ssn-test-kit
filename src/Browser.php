f8712fc82a00d426c1caa935dff7faa54302fe5c
ssnepenthe <ssnepenthe@users.noreply.github.com>
2019-02-06 14:37:52 -0700

    WIP Browser class

---
+++ src/Browser.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace SsnTestKit;
+
+use Goutte\Client as GoutteClient;
+use GuzzleHttp\Client as GuzzleClient;
+use Symfony\Component\Panther\Client as PantherClient;
+
+class Browser {
+	protected $baseUri;
+	protected $goutte;
+	protected $panther;
+	protected $withJavascript = false;
+
+	public function __construct(string $baseUri = '')
+	{
+		$this->baseUri = $baseUri;
+	}
+
+	// @todo Panther already quits on __destruct...
+	public function quit()
+	{
+		if (null !== $this->panther) {
+			$this->panther->quit();
+		}
+
+		$this->goutte = null;
+		$this->panther = null;
+	}
+
+	public function client()
+	{
+		return $this->withJavascript ? $this->panther() : $this->goutte();
+	}
+
+	public function goutte()
+	{
+		if (null === $this->goutte) {
+			$this->goutte = new GoutteClient();
+			$this->goutte->followRedirects(false);
+
+			if ($this->baseUri) {
+				$this->goutte->setClient(new GuzzleClient([
+					'allow_redirects' => false, // Always disable so browser-kit can handle redirects.
+					'base_uri' => $this->baseUri,
+					'cookies' => true,
+				]));
+			}
+		}
+
+		return $this->goutte;
+	}
+
+	public function panther() {
+		if ( ! $this->panther ) {
+			$this->panther = PantherClient::createChromeClient( null, null, [], $this->baseUri );
+		}
+
+		return $this->panther;
+	}
+
+	public function disableJavascript()
+	{
+		$this->withJavascript = false;
+
+		return $this;
+	}
+
+	public function enableJavascript()
+	{
+		$this->withJavascript = true;
+
+		return $this;
+	}
+
+	public function isJavascriptEnabled() {
+		return $this->withJavascript;
+	}
+
+	// Provides a method of avoiding having to disable JavaScript after each test...
+	public function withJavascript(\Closure $callback)
+	{
+		$this->enableJavascript();
+
+		$callback($this);
+
+		$this->disableJavascript();
+
+		return $this;
+	}
+
+	// @todo Consider simplifying method signatures a bit?
+	public function request(
+		string $method,
+		string $uri,
+		array $parameters = [],
+		array $files = [],
+		array $server = [],
+		string $content = null,
+		bool $change_history = true
+	) {
+		$this->client()->request(
+			$method, $uri, $parameters, $files, $server, $content, $change_history
+		);
+
+		// It is important to note that responses from panther don't include status and headers.
+		//
+		// It may be beneficial for the response to know which client it was generated from so that
+		// we can trigger some sort of notice if attempting to check status or headers - Or maybe we
+		// just infer this from type of crawler as there is a panther-specific crawler class.
+		//
+		// On the other hand - not totally certain it is beneficial to include both the crawler when
+		// creating a test response instance...
+		//
+		// If we keep the crawler, may akso want to also include cookie jar?
+		return new Response(
+			$this->client()->getInternalResponse(),
+			$this->client()->getCrawler()
+		);
+	}
+
+	// @todo Json variants?
+
+	public function get(
+		string $uri,
+		array $parameters = [],
+		array $files = [],
+		array $server = [],
+		string $content = null,
+		bool $change_history = true
+	) {
+		return $this->request(
+			'GET', $uri, $parameters, $files, $server, $content, $change_history
+		);
+	}
+
+	public function post(
+		$uri,
+		array $parameters = [],
+		array $files = [],
+		array $server = [],
+		string $content = null,
+		bool $change_history = true
+	) {
+		// Will throw on Panther...
+		return $this->request(
+			'POST', $uri, $parameters, $files, $server, $content, $change_history
+		);
+	}
+
+	public function waitFor(
+		string $cssSelector,
+		int $timeoutInSeconds = 30,
+		int $intervalInMilliseconds = 250
+	) {
+		if (! $this->withJavascript) {
+			throw new \RuntimeException('You must enable JavaScript to wait for an element');
+		}
+
+		// @todo Think about return value...
+		return $this->panther()->waitFor($cssSelector, $timeoutInSeconds, $intervalInMilliseconds);
+	}
+}
---
+++ tests/BrowserClientTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace SsnTestKit\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Goutte\Client as GoutteClient;
+use Psr\Http\Message\UriInterface;
+use SsnTestKit\Browser;
+use Symfony\Component\Panther\Client as PantherClient;
+
+class BrowserClientTest extends TestCase {
+	/** @test */
+	public function it_provides_the_panther_client_when_javascript_is_enabled() {
+		$browser = (new Browser())->enableJavascript();
+
+		$this->assertInstanceOf(PantherClient::class, $browser->client());
+	}
+
+	/** @test */
+	public function it_provides_the_goutte_client_when_javascript_is_disabled() {
+		$browser = new Browser();
+
+		$this->assertInstanceOf(GoutteClient::class, $browser->client());
+	}
+
+	/** @test */
+	public function it_disables_redirects_by_default_for_goutte() {
+		$browser = new Browser();
+
+		$this->assertFalse($browser->goutte()->isFollowingRedirects());
+	}
+
+	/** @test */
+	public function it_caches_client_instances() {
+		$browser = new Browser();
+
+		$this->assertSame($browser->client(), $browser->client());
+		$this->assertSame($browser->goutte(), $browser->goutte());
+		$this->assertSame($browser->panther(), $browser->panther());
+	}
+
+	/** @test */
+	public function it_allows_a_user_to_enable_javascript() {
+		$browser = new Browser();
+
+		$this->assertFalse($browser->isJavascriptEnabled());
+
+		$browser->enableJavascript();
+
+		$this->assertTrue($browser->isJavascriptEnabled());
+	}
+
+	/** @test */
+	public function it_allows_a_user_to_disable_javascript() {
+		$browser = (new Browser())->enableJavascript();
+
+		$this->assertTrue($browser->isJavascriptEnabled());
+
+		$browser->disableJavascript();
+
+		$this->assertFalse($browser->isJavascriptEnabled());
+	}
+
+	/** @test */
+	public function it_provides_a_shorthand_for_enabling_javascript_for_a_single_task() {
+		$browser = new Browser();
+
+		// JavaScript is disabled outside.
+		$this->assertFalse($browser->isJavascriptEnabled());
+
+		$browser->withJavascript(function ($browser) {
+			// JavaScript is enabled in here.
+			$this->assertTrue($browser->isJavascriptEnabled());
+		});
+
+		// JavaScript is still disabled out here :).
+		$this->assertFalse($browser->isJavascriptEnabled());
+	}
+
+	/** @test */
+	public function it_provides_a_chainable_interface_where_appropriate() {
+		$browser = new Browser();
+
+		$this->assertSame($browser, $browser->enableJavascript());
+		$this->assertSame($browser, $browser->disableJavascript());
+		$this->assertSame($browser, $browser->withJavascript(function () {}));
+	}
+}
